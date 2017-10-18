cfa556c6446732a441b3e8bea9c1bd6379ff16e9
DESKTOP-8QRH60F\Boswell Yu <yubooster@163.com>
2017-10-18 22:26:30 +0800

    Add AES encrypt function on server side

--- utility/AES.php
+++ utility/AES.php
@@ -9,7 +9,35 @@ class AES
 {
     public static function Decrypt($userId, $msgdata)
     {
-        // TODO: Encrypt the given message with session key of userId
-        return $msgdata;
+        $decryptData = $msgdata;
+        $tbPlayerLogin = new TbPlayerLogin();
+        $tbPlayerLogin->setUserId($userId);
+        if($tbPlayerLogin->loadFromExistFields()) {
+            $sessionKey = $tbPlayerLogin->getSessionKey();
+
+            $iv = $sessionKey;
+            $cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, '');
+            if(mcrypt_generic_init($cipher, $sessionKey, $iv) != -1) {
+                $decryptData = mdecrypt_generic($cipher, $msgdata);
+                $decryptData = self::trimEnd($decryptData);
+            }
+        }
+        return $decryptData;
     }
+
+    private static function trimEnd($text){
+    $len = strlen($text);
+    $c = $text[$len-1];
+
+    if(ord($c) < $len){
+        for($i=$len-ord($c); $i<$len; $i++){
+            if($text[$i] != $c)
+            {
+                return $text;
+            }
+        }
+        return  substr($text, 0, $len-ord($c));
+    }
+    return $text;
+}
 }
